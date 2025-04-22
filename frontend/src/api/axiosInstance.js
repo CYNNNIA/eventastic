76ae0fabcd22cf0fccc3173cf7ebbef38074c458
cynnnia <cynthialorenzolopez@gmail.com>
2025-04-22 20:03:08 +0200

    vercel

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -1,19 +1,15 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL:
-    process.env.NODE_ENV === 'production'
-      ? 'https://eventastic-1.onrender.com/api' // URL en producción
-      : 'http://localhost:5001/api', // URL en local
+  baseURL: process.env.REACT_APP_API_BASE_URL || 'http://localhost:5001/api',
   headers: { 'Content-Type': 'application/json' },
 });
 
-// Interceptor para añadir el token a las cabeceras de cada solicitud
 axiosInstance.interceptors.request.use(
   (config) => {
-    const token = localStorage.getItem('token');  // Obtener el token del localStorage
+    const token = localStorage.getItem('token');
     if (token) {
-      config.headers.Authorization = `Bearer ${token}`;  // Añadir el token a las cabeceras de la solicitud
+      config.headers.Authorization = `Bearer ${token}`;
     }
     return config;
   },
