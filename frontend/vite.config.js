febebae6c1c767132623c037901e92ee91ef6cab
cynnnia <cynthialorenzolopez@gmail.com>
2025-04-22 21:18:35 +0200

    vite config

---
+++ frontend/vite.config.js
@@ -0,0 +1,18 @@
+// frontend/vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
+
+export default defineConfig({
+  plugins: [react()],
+  root: '.', // ⬅️ Asegura que Vite sepa que el `index.html` está en esta raíz
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+})
