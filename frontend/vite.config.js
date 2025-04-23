6d9ca0669df670f469275affe5a8a3bf574beb6b
cynnnia <cynthialorenzolopez@gmail.com>
2025-04-23 11:56:57 +0200

    fix vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,18 +1,9 @@
-// frontend/vite.config.js
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  root: './',
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
   build: {
     outDir: 'dist',
-    emptyOutDir: true,
-  },
-});
+  }
+})
