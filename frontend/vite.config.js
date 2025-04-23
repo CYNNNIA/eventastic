d865f123e21dc32823074f7be7e3a49b2d273895
cynnnia <cynthialorenzolopez@gmail.com>
2025-04-23 13:00:38 +0200

    ..............................

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist',
+    outDir: 'dist'
   },
-  base: './' // ✅ ESTO ES IMPORTANTE PARA VERCEL
+  base: './' // ✅ Agrega esta línea
 })
