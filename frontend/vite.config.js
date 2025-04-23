1196b0aa6154ced803d51c644e9be2ae1e4b2ec4
cynnnia <cynthialorenzolopez@gmail.com>
2025-04-23 14:16:40 +0200

    nuevo vercel

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,12 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist'
+    outDir: 'dist',
+    rollupOptions: {
+      external: [
+        'fs', 'path', 'url', 'crypto', 'stream',
+        'events', 'util', 'module', 'tty', 'perf_hooks'
+      ]
+    }
   }
 })
